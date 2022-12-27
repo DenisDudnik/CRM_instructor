9f68e19ec48a0133ad63b47e4401eaeb0dfe305f
Shevchenko-Py-lab <shevchenko.logist@mail.ru>
2022-12-27 16:13:57 +0300

    CRM_reset_change_password

--- users/views.py
+++ users/views.py
@@ -1,15 +1,23 @@
 from django.conf import settings
 from django.contrib import auth
+from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import PasswordResetForm, SetPasswordForm
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.tokens import default_token_generator
 from django.contrib.auth.views import (PasswordChangeView,
                                        PasswordResetCompleteView,
                                        PasswordResetConfirmView,
                                        PasswordResetDoneView,
                                        PasswordResetView)
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, resolve_url
+from django.template.response import TemplateResponse
 from django.urls import reverse, reverse_lazy
+from django.utils.http import urlsafe_base64_decode
+from django.views.decorators.cache import never_cache
+from django.views.decorators.csrf import csrf_protect
+from django.views.decorators.debug import sensitive_post_parameters
 from django.views.generic import UpdateView
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
@@ -40,6 +48,8 @@ def login(request) -> HttpResponse:
             if user and user.is_active:
                 auth.login(request, user)
                 return HttpResponseRedirect(reverse('user_profile'))
+        else:
+            return HttpResponseRedirect(reverse('auth:password_reset'))
     form = LoginForm()
     context = {
         'title': 'Вход',
@@ -216,26 +226,126 @@ class UserPasswordEditView(PasswordChangeView):
 
 class UserPasswordResetView(PasswordResetView):
     """Initialize password reset. Enter corresponding e-mail to receive reset link"""
-    template_name = 'users/password-reset/password_reset_form.html',
-    subject_template_name = 'users/password-reset/password_reset_subject.txt',
-    email_template_name = 'users/password-reset/password_reset_email.html',
-    extra_context = {
-        'title': 'Сброс пароля',
-        'button': 'Сбросить пароль',
-    }
-    context_object_name = 'item'
+
+    def __init__(self, **kwargs):
+        super().__init__(kwargs)
+        self.POST = None
+        self.method = None
+
+    @csrf_protect
+    def password_reset(request, is_admin_site=False,
+                       template_name='/users/password-reset/password_reset_form.html',
+                       email_template_name='users/password-reset/password_reset_email.html',
+                       subject_template_name='users/password-reset/password_reset_subject.txt',
+                       password_reset_form=PasswordResetForm,
+                       token_generator=default_token_generator,
+                       post_reset_redirect=None,
+                       from_email=None,
+                       extra_context=None):
+        if post_reset_redirect is None:
+            post_reset_redirect = reverse('auth:password_reset_done')
+        else:
+            post_reset_redirect = resolve_url(post_reset_redirect)
+        if request.method == "POST":
+            form = password_reset_form(request.POST)
+            if form.is_valid():
+                opts = {
+                    'token_generator': token_generator,
+                    'from_email': from_email,
+                    'email_template_name': email_template_name,
+                    'subject_template_name': subject_template_name,
+                    'request': request,
+                }
+                if is_admin_site:
+                    opts = dict(opts, domain_override=request.get_host())
+                form.save(**opts)
+                return HttpResponseRedirect(post_reset_redirect)
+        else:
+            form = password_reset_form()
+        context = {
+            'form': form,
+        }
+        if extra_context is not None:
+            context.update(extra_context)
+        return TemplateResponse(request, template_name, context)
+
+    def get_host(self):
+        pass
 
 
 class UserPasswordResetDoneView(PasswordResetDoneView):
     """Automatically displays after submitting an e-mail"""
-    template_name = 'users/password-reset/password_reset_done.html'
+    def password_reset_done(request,
+                            template_name='users/password-reset/password_reset_done.html',
+                            current_app=None, extra_context=None):
+        context = {}
+        if extra_context is not None:
+            context.update(extra_context)
+        return TemplateResponse(request, template_name, context)
 
 
 class UserPasswordResetConfirmView(PasswordResetConfirmView):
     """This link is emailed to the user. Here token is validated against user data."""
-    template_name = 'users/password-reset/password_reset_confirm.html'
+
+    def __init__(self, **kwargs):
+        super().__init__(kwargs)
+        self.POST = None
+        self.method = None
+
+    @sensitive_post_parameters()
+    @never_cache
+    def password_reset_confirm(request, uidb64=None, token=None,
+                               template_name='users/password-reset/password_reset_confirm.html',
+                               token_generator=default_token_generator,
+                               set_password_form=SetPasswordForm,
+                               post_reset_redirect=None,
+                               current_app=None, extra_context=None):
+        """
+        View that checks the hash in a password reset link and presents a
+        form for entering a new password.
+        """
+        user = get_user_model()
+        assert uidb64 is not None
+        assert token is not None
+        if post_reset_redirect is None:
+            post_reset_redirect = reverse('auth:password_reset_complete')
+        else:
+            post_reset_redirect = resolve_url(post_reset_redirect)
+        try:
+            uid = urlsafe_base64_decode(uidb64)
+            user = user.get(pk=uid)
+        except (TypeError, ValueError, OverflowError, user.DoesNotExist):
+            user = None
+
+        if user is not None and token_generator.check_token(user, token):
+            valid_link = True
+            if request.method == 'POST':
+                form = set_password_form(user, request.POST)
+                if form.is_valid():
+                    form.save()
+                    return HttpResponseRedirect(post_reset_redirect)
+            else:
+                form = set_password_form(None)
+        else:
+            valid_link = False
+            form = None
+        context = {
+            'form': form,
+            'validlink': valid_link,
+        }
+        if extra_context is not None:
+            context.update(extra_context)
+        return TemplateResponse(request, template_name, context)
 
 
 class UserPasswordResetCompleteView(PasswordResetCompleteView):
     """Page Display after successful Password Reset."""
-    template_name = 'users/password-reset/password_reset_complete.html'
+    def password_reset_complete(request,
+                                template_name='users/password-reset/password_reset_complete.html',
+                                current_app=None, extra_context=None):
+        context = {
+            'login_url': resolve_url(settings.LOGIN_URL)
+        }
+        if extra_context is not None:
+            context.update(extra_context)
+        return TemplateResponse(request, template_name, context)
